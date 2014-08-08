c901280075b1e7a24c9d55af194822f52916399e
James Manton <ajd.manton@googlemail.com>
2014-08-08 21:44:27 +0100

    Initialise with first version of package

---
+++ R/multiply.R
@@ -0,0 +1,10 @@
+#' Multiply two numbers together
+#' 
+#' @param x a number.
+#' @param y another number.
+#' 
+#' @return The product of \code{x} and \code{y}.
+#' @export
+multiply_numbers <- function(x, y) {
+  x * y
+}
---
+++ tests/testthat/test-multiplication.R
@@ -0,0 +1,5 @@
+context("Multiplication")
+
+test_that("we can multiply numbers correctly", {
+  expect_equal(2*5, multiply_numbers(2, 5))
+})
